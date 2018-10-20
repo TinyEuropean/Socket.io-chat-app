579384cb3a538d0ed52f0823fc88352ab8a1ace7
Essau Tesfay <essautesfay.te@gmail.com>
2018-10-20 19:49:30 +0200

    finaleee changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var io = require('socket.io').listen(server);
 users = [];
 connections = [];
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 console.log('Server running...');
 
 app.get('/', function(req, res){
@@ -20,14 +20,15 @@ console.log('Connected: %s sockets connected', connections.length);
    // Disconnect 
    socket.on('disconnect', function(data){
        if(!socket.username) return;
-       users.splice(users)
+       users.splice(users.indexOf(socket.username), 1);
+       updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
     console.log('Disconnected: %s sockets connected', connections.length);
    });
 
    // Send Message
    socket.on('send message',function(data){
-       io.sockets.emit('new message', {msg: data});
+       io.sockets.emit('new message', {msg: data, user: socket.username});
     });
 
     // New User
@@ -39,6 +40,6 @@ console.log('Connected: %s sockets connected', connections.length);
     });
 
     function updateUsernames(){ 
-        io.socket.emit('get users', usernames);
+        io.sockets.emit('get users', users);
     }
 });
